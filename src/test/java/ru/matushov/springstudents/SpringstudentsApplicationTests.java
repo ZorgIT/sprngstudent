95c5de5bbdb740d92c5e7a6361f0c6890c183420
Zorgit <piligrimexe@Gmail.com>
2024-02-11 12:38:24 +0300

    Initial Spring boot 3.2.2 state

---
+++ src/test/java/ru/matushov/springstudents/SpringstudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matushov.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
